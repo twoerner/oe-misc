b39653618743bf7778622e549f41c2bdb35dd359
Trevor Woerner <twoerner@gmail.com>
2017-01-11 12:24:45 -0500

    scripts/builds_yocto_docs.sh: build HTML-only docs
    There are two documents (meta-manual and yocto-project-qs) that are only
    available in HTML format. Add support to build these two documents as well.
    
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

--- scripts/build_yocto_docs.sh
+++ scripts/build_yocto_docs.sh
@@ -13,6 +13,17 @@ else
 fi
 popd > /dev/null 2>&1
 
+echo "yocto html-only docs"
+pushd yocto-docs/documentation > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "can't find yocto docs"
+else
+	for HTML in mega-manual yocto-project-qs; do
+		make html DOC=$HTML
+	done
+fi
+popd > /dev/null 2>&1
+
 echo ""
 echo "bitbake doc"
 pushd bitbake/doc > /dev/null 2>&1
